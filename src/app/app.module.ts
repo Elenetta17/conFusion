b2cf78b866bcceb14ebeb749f3d890f6461b3dbe
Elena NARDI <elena.nardi@oldi.fr>
2022-04-22 13:16:40 +0200

    Assignment 3 Task 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MatCheckboxModule } from '@angular/material/checkbox';
 import { FormsModule } from '@angular/forms'; 
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
+
 import { AppComponent } from './app.component';
 
 import {MatListModule} from '@angular/material/list'
@@ -31,6 +32,7 @@ import { ContactComponent } from './contact/contact.component';
 import { AppRoutingModule} from './app-routing/app-routing.module';
 import { MatSelectModule} from '@angular/material/select';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import {MatSliderModule} from '@angular/material/slider'; 
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { DishService } from './services/dish.service';
@@ -64,6 +66,7 @@ import { LoginComponent } from './login/login.component';
     MatProgressSpinnerModule,
     MatSelectModule,
     MatSlideToggleModule,
+    MatSliderModule,
     AppRoutingModule,
     MatDialogModule,
     MatFormFieldModule, 
--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -9,6 +9,7 @@ import {Location} from '@angular/common';
 import { switchMap } from 'rxjs/operators';
 
 import {Comment} from '../shared/comment';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-dishdetail',
@@ -19,6 +20,7 @@ export class DishdetailComponent implements OnInit {
 
   @Input()
   dish: Dish;
+
   dishIds: string[];
   prev: string;
   next: string;
@@ -32,6 +34,22 @@ export class DishdetailComponent implements OnInit {
   commentForm: FormGroup;
   commentInput: Comment;
 
+  formErrors : any ={
+    'author': '',
+    'comment': ''
+  }
+  validationMessages: any = {
+    'author': {
+        'required': 'Name is required',
+        'minLength': 'Name must be at least 2 characters long'
+    },
+    'comment':  {
+        'required': 'Comment is required',
+    }
+  }
+
+
+
   ngOnInit(): void {
     this.dishservice.getDishIds().subscribe(dishIds => this.dishIds = dishIds);
     this.route.params.pipe(switchMap((params: Params) => this.dishservice.getDish(params['id'])))
@@ -41,11 +59,51 @@ export class DishdetailComponent implements OnInit {
 
 createcommentForm() {
     this.commentForm = this.fc.group({
-      rating: '',
+      rating: '5',
       comment: ['', Validators.required],
       author: ['', Validators.required, Validators.minLength(2)],
-      date: '',
+      date: Date.now(),
     });
+
+    this.commentForm.valueChanges
+      .subscribe(data => this.onValueChanged(data));
+
+    this.onValueChanged(); // (re)set validation messages now
+  }
+
+   onValueChanged(data?: any) {
+    if (!this.commentForm) { return; }
+    const form = this.commentForm;
+    for (const field in this.formErrors) {
+      if (this.formErrors.hasOwnProperty(field)) {
+        // clear previous error message (if any)
+        this.formErrors[field] = '';
+        const control = form.get(field);
+        if (control && control.dirty && !control.valid) {
+          const messages = this.validationMessages[field];
+          for (const key in control.errors) {
+            if (control.errors.hasOwnProperty(key)) {
+              this.formErrors[field] += messages[key] + ' ';
+            }
+          }
+        }
+      }
+    }
+  }
+
+  onSubmit() {
+    this.commentInput = this.commentForm.value;
+    console.log(this.commentInput);
+    this.commentForm.reset({
+    firstname: '',
+    lastname: '',
+    telnum: '',
+    email: '',
+    agree: false,
+    ontacttype: 'None',
+    message: ''
+    });
+    this.commentFormDirective.resetForm();
   }
 
 
@@ -68,48 +126,3 @@ setPrevNext(dishId: string){
                 date: '2012-10-16T17:57:28.556094Z'
             },*/
 
-  const DISH = {
-    id: '0',
-    name: 'Uthappizza',
-    image: '/assets/images/uthappizza.png',
-    category: 'mains',
-    featured: true,
-    label: 'Hot',
-    price: '4.99',
-    // tslint:disable-next-line:max-line-length
-    description: 'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',
-    comments: [
-       {
-           rating: 5,
-           comment: 'Imagine all the eatables, living in conFusion!',
-           author: 'John Lemon',
-           date: '2012-10-16T17:57:28.556094Z'
-       },
-       {
-           rating: 4,
-           comment: 'Sends anyone to heaven, I wish I could get my mother-in-law to eat it!',
-           author: 'Paul McVites',
-           date: '2014-09-05T17:57:28.556094Z'
-       },
-       {
-           rating: 3,
-           comment: 'Eat it, just eat it!',
-           author: 'Michael Jaikishan',
-           date: '2015-02-13T17:57:28.556094Z'
-       },
-       {
-           rating: 4,
-           comment: 'Ultimate, Reaching for the stars!',
-           author: 'Ringo Starry',
-           date: '2013-12-02T17:57:28.556094Z'
-       },
-       {
-           rating: 2,
-           comment: 'It\'s your birthday, we\'re gonna party!',
-           author: '25 Cent',
-           date: '2011-12-02T17:57:28.556094Z'
-       }
-   ]
-
-
-};
